ae159f2a1627c2e9d9c48473b64a2faa238efa31
wyc <bemyself008@163.com>
2023-02-10 14:39:01 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,7 @@
+# 打包
+  npm run build:dev ------测试
+  npm run build:prod -----正式
+
+
+# 运行
+  dist/sharingtransfer.html
---
+++ src/config.js
@@ -0,0 +1,46 @@
+export const config = [
+  {
+    id:'49',
+    website:'https://www.heiheilove.com',
+    title:'甜芯',
+    src:'//cgwh.t4m.cn/applink.js'
+  },
+  {
+    id:'50',
+    website:'http://fujinai.heiheilove.com',
+    title:'附近爱',
+    src:'//cgwh.t50m.cn/applink.js'
+  },
+  {
+    id:'51',
+    website:'http://www.imbanbanapp.com',
+    title:'花知',
+    src:'//cgwh.t51m.cn/applink.js'
+  },
+  {
+    id:'52',
+    website:'http://www.xiaomianju.cn',
+    title:'小面具',
+    src:'//cgwh.t52m.cn/applink.js'
+  },
+  {
+    id:'53',
+    website:'http://xingyou.heiheilove.com',
+    title:'星尤交友',
+    src:'//cgwh.t53m.cn/applink.js'
+  },
+  {
+    id:'54',
+    website_android:'http://tc.xiaomianju.cn',// andorid跳转
+    website_ios:'http://shudong.xsky.online',// ios跳转
+    title:'同城交友圈',
+    src:'//cgwh.t54m.cn/applink.js'
+  },
+  {
+    id:'55',
+    website_android:'http://tc.xiaomianju.cn',// andorid跳转
+    website_ios:'http://shudong.xsky.online',// ios跳转
+    title:'树洞吧',
+    src:'//cgwh.t4m.cn/applink.js'
+  }
+]
